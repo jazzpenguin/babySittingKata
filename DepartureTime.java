97b950617d18d88bbeee7f3fb12ce899a08020f1
jazzpenguin <charlesmartinj@gmail.com>
2017-03-03 17:32:48 -0600

    Added .isBefore to departureTime
    .isBefore checks to see if the time is  before 04:00.

--- DepartureTime.java
+++ DepartureTime.java
@@ -9,7 +9,6 @@ public class DepartureTime {
     public LocalTime getLeaveBeforeFour() {
         return leaveBeforeFour;
     }
-
     public LocalTime getLeaveAfterFour() {
         return leaveAfterFour;
     }
@@ -20,11 +19,17 @@ public class DepartureTime {
         return departureTimeCheck();
     }
 
-    LocalTime leaveBeforeFour = LocalTime.parse("04:00");
+    LocalTime leaveBeforeFour = LocalTime.parse("02:00");
     LocalTime leaveAfterFour = LocalTime.parse("04:30");
     LocalTime noLaterThanFourDeparture = LocalTime.parse("04:00");
 
     private String departureTimeCheck() throws ParseException {
+        if (getLeaveBeforeFour().isBefore(getNoLaterThanFourDeparture())){
+            System.out.println("Have a nice night!");
+        }
+        else{
+            System.out.println("It is after 04:00, your pay stopped at 04:00.");
+        }
 
 return null;
     }
--- LeaveNoLaterThanFourTest.java
+++ LeaveNoLaterThanFourTest.java
@@ -1,23 +1,19 @@
 package com.example;
-
 import org.junit.jupiter.api.Test;
-
 import java.text.ParseException;
 import java.time.LocalTime;
-
 import static org.junit.jupiter.api.Assertions.*;
 
+
+
 class LeaveNoLaterThanFourTest {
-
-
-
     @Test
     void testLeaveNoLaterThanFour() throws ParseException {
         DepartureTime lt = new DepartureTime();
 
         lt.getDepartureTimeCheck();
 
-        assertEquals(lt.getLeaveBeforeFour(), LocalTime.parse("04:00"));
+        assertEquals(lt.getLeaveBeforeFour(), LocalTime.parse("02:00"));
         assertNotEquals(lt.getLeaveAfterFour(), lt.getNoLaterThanFourDeparture());
     }
 }
