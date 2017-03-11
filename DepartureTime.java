5ed33cbb1fecc11a19734583fa1df8edab817261
jazzpenguin <charlesmartinj@gmail.com>
2017-03-11 17:02:33 -0600

    Dried out DepartureTime

--- DepartureTime.java
+++ DepartureTime.java
@@ -1,40 +1,32 @@
 package com.example;
+import sun.util.resources.cldr.ka.LocaleNames_ka;
 
-import java.text.ParseException;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.Scanner;
+import java.time.format.DateTimeParseException;
+
 
 
 public class DepartureTime {
-    Scanner in = new Scanner(System.in);
 
-    public LocalTime getBabySitterDepartureTime() {
-        return babySitterDepartureTime;
+    String beforeFour = "Have a nice night!";
+    String afterFour= "It is after 04:00, your pay stopped at 04:00.";
+
+    LocalDateTime noLaterThanFourDeparture = LocalDateTime.of(2017, 01, 02,04,00);
+
+    public LocalDateTime babySitterDepartureTime(){
+        LocalDateTime departureTime = LocalDateTime.of(2017, 01, 02, 03, 59);
+        return departureTime;
     }
-        public LocalTime getNoLaterThanFourDeparture(){
-            return noLaterThanFourDeparture;
+
+    public String departureTimeCheck(LocalDateTime time){
+        String clockingOut = checkedOut(babySitterDepartureTime().isBefore(noLaterThanFourDeparture)) ? afterFour : beforeFour;
+        return clockingOut;
     }
-    public String getDepartureTimeCheck() throws ParseException {
-        return departureTimeCheck();
+    private boolean checkedOut(boolean clockedOut){
+        return clockedOut==clockedOut || "".equals(beforeFour);
     }
 
-    LocalTime babySitterDepartureTime = LocalTime.parse(in.next());
-    LocalTime noLaterThanFourDeparture = LocalTime.parse("04:00");
-
-    private String departureTimeCheck() throws ParseException {
-        if (getBabySitterDepartureTime().isBefore(getNoLaterThanFourDeparture())){
-            System.out.println("Have a nice night!");
-        }
-        else{
-            System.out.println("It is after 04:00, your pay stopped at 04:00.");
-        }
-
-return null;
     }
 
 
-
-
-
-
-}
