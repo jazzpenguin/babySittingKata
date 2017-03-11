c8dc1dc311b6e791ff357424d8274c3d2ed46edc
jazzpenguin <charlesmartinj@gmail.com>
2017-03-11 16:06:08 -0600

    Dried out ArrivalTime

--- ArrivalTime.java
+++ ArrivalTime.java
@@ -1,35 +1,30 @@
 package com.example;
 import java.text.ParseException;
 import java.time.LocalTime;
-import java.util.Scanner;
+
 
 public class ArrivalTime {
-    Scanner in = new Scanner(System.in);
 
-    public LocalTime getBabySitterArrivalTime() {
+    public static LocalTime getBabySitterArrivalTime(){
+        LocalTime babySitterArrivalTime = LocalTime.of(16, 59);
         return babySitterArrivalTime;
     }
 
-       public String getArrivalTimeCheck() throws ParseException {
-        return arrivalTimeCheck();
+    public static LocalTime requestedArrivalTime = LocalTime.of(17, 00);
+    public static final String clockedIn = "You are clocked in";
+    public static final String notClockedIn = " You need to wait until 17:00 to clock in.";
+
+
+    public static String arrivalTimeCheck(LocalTime time) {
+        String clockingIn = checkedIn(getBabySitterArrivalTime().isBefore(requestedArrivalTime)) ? notClockedIn : clockedIn;
+
+        return clockingIn;
     }
-
-    LocalTime babySitterArrivalTime = LocalTime.parse(in.next());
-
-    LocalTime requestedArrivalTime = LocalTime.parse("17:00");
-
-    private String arrivalTimeCheck() throws ParseException {
-        String clockedIn = "You are clocked in";
-        String notClockedIn = "The current time is: " + getBabySitterArrivalTime() + " You need to wait until 17:00 to clock in.";
-//checks to see if the babysitters clock in time is before 5:00 pm. if it isn't, the babysitter is clocked in.
-        if (babySitterArrivalTime.isBefore(requestedArrivalTime)) {
-            System.out.println(notClockedIn);
-            System.out.println("This program will close now. Run again at 17:00.");
-            System.exit(0);
-            return notClockedIn;
-        } else {
-            System.out.println(clockedIn);
-            return clockedIn;
+    private static boolean checkedIn(boolean time) {
+        return time == time || "".equals(clockedIn);
         }
     }
-}
+
+
+
+
